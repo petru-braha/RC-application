296a2f132a87e21787248f1b5eb52ede8afc8557
p <petrubraha@gmail.com>
2025-12-05 19:37:56 +0200

    removed: non-branch modules from __init__ file

--- src/protocol/__init__.py
+++ src/protocol/__init__.py
@@ -1,6 +1,3 @@
 from .parser import parser
-from .encoder import encoder
-from .sanitizer import sanitizer
-from .decoder import decoder
 
-__all__ = ["parser", "encoder", "sanitizer", "decoder"]
+__all__ = ["parser"]
