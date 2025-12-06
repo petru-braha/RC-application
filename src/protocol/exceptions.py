41e31988dd2c48c83fb80385750bf65c5e1a9d93
p <petrubraha@gmail.com>
2025-12-06 23:20:32 +0200

    fix: changed generic error message of base exception class

--- src/protocol/exceptions.py
+++ src/protocol/exceptions.py
@@ -3,9 +3,12 @@ class RCException(Exception):
     
     def __str__(self) -> str:
         argc = len(self.args)
-        msg = "Abstract error."
+        # If argc == 0 (an exception was raised with no arguments).
+        msg = "Generic error with no message."
         if argc == 1:
             msg = self.args[0]
+            if isinstance(msg, str) is False:
+                msg = str(msg)
         elif argc > 1:
             msg = str(self.args)
         return self.MSG_PREFIX + " - " + msg
