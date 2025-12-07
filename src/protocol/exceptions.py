511d767b4e163e263f7b1a5e822256da43ed794e
Petru Bogdan Brahă <petrubraha@gmail.com>
2025-12-07 15:03:02 +0200

    feat: parser module (#1)
    * feat: fully developed,tested,documented parser module
    
    * removed: non-branch modules from __init__ file
    
    * fix: added comma in the constant list
    
    * feat: parser exceptions
    
    * fix: changed generic error message of base exception class
    
    * removed: 2 word commands support
    
    * removed: todo comment

---
+++ src/protocol/exceptions.py
@@ -0,0 +1,24 @@
+class RCException(Exception):
+    MSG_PREFIX = "Redis Client Exception"
+    
+    def __str__(self) -> str:
+        argc = len(self.args)
+        # If argc == 0 (an exception was raised with no arguments).
+        msg = "Generic error with no message."
+        if argc == 1:
+            msg = self.args[0]
+            if isinstance(msg, str) is False:
+                msg = str(msg)
+        elif argc > 1:
+            msg = str(self.args)
+        return self.MSG_PREFIX + " - " + msg
+
+#! parser errors
+class ParserError(RCException):
+    MSG_PREFIX = "Parser exception"
+
+class SpaceError(ParserError):
+    pass
+
+class QuoteError(ParserError):
+    pass
