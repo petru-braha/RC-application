e8bc355ca89ab29382290e1e20b0d1c4a30e62c5
p <petrubraha@gmail.com>
2025-12-08 19:13:25 +0200

    idea: recursive approach of sanitizer

--- src/protocol/sanitizer.py
+++ src/protocol/sanitizer.py
@@ -1,58 +1,38 @@
 from frozendict import frozendict
-from .constants_cmds import CmdDef, CONNECTION_CMDS, CMDS
+from .cmds import *
 from .exceptions import ArgumentCountError
 
-def _count_opt(argv: list[str], cmd_def: CmdDef) -> int:
-    """
-    Counts how many optional command options appear in the provided argument list.
+def _validate_int(arg: str) -> None:
+    try:
+        int(arg)
+    except:
+        raise TypeError("")
 
-    Parameters:
-        cmd_def (CmdDef): The command definition object containing optional options
-                          in its `opt` attribute.
+def _validate_flt(arg) -> int:
+    pass
+def _validate_str(arg) -> int:
+    pass
+def _validate_args(arg_def, arg_input) -> int:
+    pass
+def _validate_opts(arg_def, arg_input) -> int:
+    pass
+def _validate_opt_set(arg_def, arg_input) -> int:
+    pass
 
-    Returns:
-        int: The number of optional option keywords found in `argv`.
-    """
-    if cmd_def.opt == None:
-        return 0
-    
-    optc = 0
-    # Here arg stands for input argument.
-    for arg in argv:
-        # cmd_def.opt here is a list of dicts.
-        for opt in cmd_def.opt:
-            if arg in opt:
-                optc += opt[arg] + 1
-    return optc
-
-def _validate_argc(cmd: str, argv: list[str], cmd_dict: frozendict[str, CmdDef]) -> None:
-    """
-    Validates that the number of arguments provided for a Redis command
-    meets the minimum required count, ignoring optional arguments.
-
-    Parameters:
-        cmd (str): The Redis command name.
-        argv (arr): List of arguments provided for the command.
-        cmd_dict: Mapping of command names to RedisCommand objects, used to
-             retrieve metadata about required and optional arguments.
-
-    Raises:
-        ArgumentCountError: If the number of arguments (excluding optional options)
-                            is less than the required minimum for the command.
-    """
-    argc = len(argv)
-    cmd_def = cmd_dict[cmd]
-    
-    # Options count.
-    optc = _count_opt(argv, cmd_def)
-    # Options are not required.
-    # Their count shouldn't make up for the missing required arguments.
-    argc -= optc
-    if argc < cmd_def.required_argc:
-        raise ArgumentCountError("Missing required arguments: only {argc} provided.")
-
-    # todo check pair count
-    # todo check required after pairs count
+def _validate_arg(arg_def: Arg, argv: list[str], arg_idx):
+    if isinstance(arg_def, IntArg):
+        _validate_int(argv[arg_idx])
+        return arg_idx + 1
+    if isinstance(arg_def, FltArg):
+        pass
+    if isinstance(arg_def, StrArg):
+        pass
+    if isinstance(arg_def, Args):
+        pass
+    if isinstance(arg_def, Opts):
+        pass
+    if isinstance(arg_def, OptSet):
+        pass
 
 def sanitizer(cmd: str, argv: list[str]) -> None:
     """
@@ -68,10 +48,18 @@ def sanitizer(cmd: str, argv: list[str]) -> None:
         KeyError: If the command is invalid.
         ArgumentCountError: If the input arguments for an command are either missing or too many.
     """
-    argc = len(argv)
+    cmd_dict = None
     for cmd_dict in CMDS:
         if cmd not in cmd_dict:
             continue
-        _validate_argc(cmd, argv, cmd_dict)
+        cmd_dict = cmd_dict
         break
-    raise KeyError("Command is either not supported or not existent.")
+    
+    if cmd_dict == None:
+        raise KeyError("Command is either not supported or not existent.")
+
+    cmd_args_def = cmd_dict[cmd]
+    arg_idx = 0
+    for arg_section in cmd_args_def:
+        arg_idx = _validate_arg(arg_section, argv, arg_idx)
+
