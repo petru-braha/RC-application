83ed25a1b8b7893d06867f202a11ed41354adc01
p <petrubraha@gmail.com>
2025-12-15 17:00:31 +0200

    removed: sanitizer

--- src/protocol/__init__.py
+++ src/protocol/__init__.py
@@ -1,6 +1,5 @@
 from .parser import parser
 from .encoder import encoder
-from .sanitizer import sanitizer
 from .decoder import decoder
 
-__all__ = ["parser", "encoder", "sanitizer", "decoder"]
+__all__ = ["parser", "encoder", "decoder"]
