6b6329849ce3039961e42cf6d2f6da0dfa215ba6
p <petrubraha@gmail.com>
2025-12-15 17:05:17 +0200

    tst: immutable base interface

---
+++ tst/test_immutable.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+
+from src.exceptions import AssignmentError
+from src.immutable import Immutable
+
+        
+class TestImmutable(TestCase):
+
+    class TestObject(Immutable):
+        def __init__(self, key: str, value: int):
+            self.key = key
+            self.value = value
+
+    def test_initialization_success(self):
+        obj = TestImmutable.TestObject("test", 0)
+        self.assertEqual(obj.key, "test")
+        self.assertEqual(obj.value, 0)
+        
+    def test_assignment_failure(self):
+        obj = TestImmutable.TestObject("test", 1)
+        with self.assertRaises(AssignmentError):
+            obj.key = "cool"
+        with self.assertRaises(AssignmentError):
+            obj.value = 1
