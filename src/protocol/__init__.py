c17edd5674326ce3ed32da9714a20cdfbba9b4c3
Petru Bogdan Brahă <petrubraha@gmail.com>
2025-12-20 20:23:18 +0200

    formatter-module (#7)
    * feat: output formatter module (pretty print)
    
    * tst: formatter module
    
    * fix: moved constants in another file
    
    * doc: constant
    
    * feat: constant type annotations
    
    * fix: constant name
    
    * fix: constant name

--- src/protocol/__init__.py
+++ src/protocol/__init__.py
@@ -1,5 +1,6 @@
 from .parser import parser
 from .encoder import encoder
 from .decoder import decoder
+from .formatter import formatter
 
-__all__ = ["parser", "encoder", "decoder"]
+__all__ = ["parser", "encoder", "decoder", "formatter"]
