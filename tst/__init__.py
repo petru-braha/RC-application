9f9581c004a32f387e65adb5679d69a6b831ec22
p <petrubraha@gmail.com>
2025-12-26 21:05:41 +0200

    fix: moved protocol tests into a nested directory

--- tst/__init__.py
+++ tst/__init__.py
@@ -1,6 +1,1 @@
-from .test_parser import TestParser
-from .test_encoder import TestEncoder
-from .test_decoder import TestDecoder
-from .test_formatter import TestFormatter
-
-__all__ = ["TestParser", "TestEncoder", "TestDecoder", "TestFormatter"]
+# Including `__init__.py` files in all subdirectories is required for the coverage module to identify and track nested tests.
