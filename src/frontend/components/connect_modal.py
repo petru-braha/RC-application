b41913d9ec807e3f1c648377f69fdbe36cb9ddf8
p <petrubraha@gmail.com>
2026-01-03 17:38:33 +0200

    refactor: modals

--- src/frontend/components/connect_modal.py
+++ src/frontend/components/connect_modal.py
@@ -2,14 +2,26 @@ from typing import Callable
 
 from frontend import PresenceChangeable
 
-from .modals import ManualMode, UrlMode
+from .modals import ManualConnect, UrlMode
 
-class ConnectModal(PresenceChangeable, ManualMode, UrlMode):
+class ModalController(PresenceChangeable):
     """
     Handles the creation and the deletion of connections through the reactor.
     """
 
-    def __init__(self, on_agenda_insert: Callable, on_agenda_remove: Callable, on_chat_insert: Callable):
-        ManualMode.__init__(self, on_agenda_insert, on_agenda_remove, on_chat_insert)
-        UrlMode.__init__(self, on_agenda_insert, on_agenda_remove, on_chat_insert)
-        self.set_url_mode()
+    def __init__(self, on_agenda_insert: Callable, on_agenda_remove: Callable, on_chat_insert: Callable, on_chat_remove: Callable):
+        
+        self.manual_modal = ManualConnect(on_agenda_insert, on_agenda_remove, on_chat_insert, on_chat_remove)
+        self.url_modal = UrlMode(on_agenda_insert, on_agenda_remove, on_chat_insert, on_chat_remove)
+        self.manual_modal.hide()
+        self.url_modal.hide()
+
+    def switch_modal(self, e):
+        if self.manual_modal.visible:
+            self.manual_modal.hide()
+            self.url_modal.show()
+        else:
+            self.manual_modal.show()
+            self.url_modal.hide()
+
+    
