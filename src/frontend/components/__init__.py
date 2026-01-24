ae78d185ee605dcd51db9c4ce76cedfaa11d28cb
P <petrubraha@gmail.com>
2026-01-24 21:55:41 +0200

    feat: frontend interfaces

--- src/frontend/components/__init__.py
+++ src/frontend/components/__init__.py
@@ -1,5 +1,4 @@
 from .chat import Chat
 from .conn_box import ConnBox
-from .interfaces import PresenceChangeable
 
-__all__ = ["Chat", "ConnBox", "PresenceChangeable"]
+__all__ = ["Chat", "ConnBox"]
