af58325e41d735437c4df5da5fbddade8146d718
P <petrubraha@gmail.com>
2026-01-24 22:46:11 +0200

    tst refactor: 100 frontend coverage

--- tst/frontend/test_layout.py
+++ tst/frontend/test_layout.py
@@ -4,27 +4,25 @@ from unittest.mock import MagicMock, patch
 from src.frontend.layout import Layout
 
 class TestLayout(TestCase):
-    agenda_patch = patch("src.frontend.layout.Agenda")
+
+    agendaframe_patch = patch("src.frontend.layout.AgendaFrame")
     chatframe_patch = patch("src.frontend.layout.ChatFrame")
-    modal_patch = patch("src.frontend.layout.ModalController")
-    leftpanel_patch = patch("src.frontend.layout.LeftPanel")
+    modal_patch = patch("src.frontend.layout.Modal")
         
     def setUp(self):
-        self.mock_agenda_cls = TestLayout.agenda_patch.start()
         self.mock_chatframe_cls = TestLayout.chatframe_patch.start()
         self.mock_modal_cls = TestLayout.modal_patch.start()
-        self.mock_leftpanel_cls = TestLayout.leftpanel_patch.start()
+        self.mock_agendaframe_cls = TestLayout.agendaframe_patch.start()
         
-        self.layout = Layout()
+        self.add_conn_callback = MagicMock()
+        self.layout = Layout(self.add_conn_callback)
 
     def tearDown(self):
-        TestLayout.agenda_patch.stop()
         TestLayout.chatframe_patch.stop()
         TestLayout.modal_patch.stop()
-        TestLayout.leftpanel_patch.stop()
+        TestLayout.agendaframe_patch.stop()
 
     def test_init(self):
-        self.mock_agenda_cls.assert_called()
         self.mock_chatframe_cls.assert_called()
-        self.mock_modal_cls.assert_called()
-        self.mock_leftpanel_cls.assert_called()
+        self.mock_modal_cls.assert_called_with(add_conn_callback=self.add_conn_callback)
+        self.mock_agendaframe_cls.assert_called()
